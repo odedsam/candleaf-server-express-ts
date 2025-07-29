6c698dfc40a6a1f5e75acf7e5569ca3819509b5d
fragged-up <154810298+fragged-up@users.noreply.github.com>
2025-07-29 07:01:16 +0300

    before-revert

--- dist/modules/v1/admin/admin.routes.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const admin_controller_1 = require("./admin.controller");
-const router = (0, express_1.Router)();
-router.get("/orders", admin_controller_1.getAllOrders);
-router.patch("/orders/:orderId/status", admin_controller_1.updateOrderStatus);
-exports.default = router;
-//# sourceMappingURL=admin.routes.js.map
