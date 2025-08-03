788523fcb66f4394f13b4055ad56e354026905d6
fragged-up <154810298+fragged-up@users.noreply.github.com>
2025-08-03 11:23:27 +0300

    rm-ssl

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,6 +1,8 @@
+import type { Express } from "express";
 import { ENV } from "./env";
 import mongoose from "mongoose";
-
+import session from "express-session";
+import MongoStore from "connect-mongo";
 
 export const connectDB = async (): Promise<void> => {
   try {
@@ -14,3 +16,24 @@ export const connectDB = async (): Promise<void> => {
   }
 };
 
+export const configureSession = (app: Express): void => {
+  app.use(
+    session({
+      secret: ENV.SESSION_SECRET,
+      resave: false,
+      saveUninitialized: false,
+      store: MongoStore.create({
+        mongoUrl: ENV.MONGO_URI,
+        collectionName: "sessions",
+        mongoOptions: {
+          serverSelectionTimeoutMS: 20000,
+        },
+      }),
+      cookie: {
+        maxAge: 24 * 60 * 60 * 1000,
+        httpOnly: true,
+        secure: ENV.NODE_ENV === "production",
+      },
+    })
+  );
+};
