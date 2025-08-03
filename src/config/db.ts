e71e95a0be9831895a2ad408c00c81cd0ad2e09c
fragged-up <154810298+fragged-up@users.noreply.github.com>
2025-08-03 11:19:39 +0300

    remove-sess

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,12 +1,10 @@
-import type { Express } from "express";
 import { ENV } from "./env";
 import mongoose from "mongoose";
-import session from "express-session";
-import MongoStore from "connect-mongo";
+
 
 export const connectDB = async (): Promise<void> => {
   try {
-    await mongoose.connect(ENV.MONGO_URI, {
+    await mongoose.connect(ENV.MONGO_URI!, {
       serverSelectionTimeoutMS:20000,
     });
     console.log("Connected to MongoDB");
@@ -16,24 +14,3 @@ export const connectDB = async (): Promise<void> => {
   }
 };
 
-export const configureSession = (app: Express): void => {
-  app.use(
-    session({
-      secret: ENV.SESSION_SECRET,
-      resave: false,
-      saveUninitialized: false,
-      store: MongoStore.create({
-        mongoUrl: ENV.MONGO_URI,
-        collectionName: "sessions",
-        mongoOptions: {
-          serverSelectionTimeoutMS: 20000,
-        },
-      }),
-      cookie: {
-        maxAge: 24 * 60 * 60 * 1000,
-        httpOnly: true,
-        secure: ENV.NODE_ENV === "production",
-      },
-    })
-  );
-};
